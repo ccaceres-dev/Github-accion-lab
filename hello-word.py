23be3b5a47ce35b5975f9b0256c39a70fb27bbd6
ccaceres-dev <78009244+ccaceres-dev@users.noreply.github.com>
2026-02-24 21:15:16 -0500

    Update hello-word.py to use environment variable

--- hello-word.py
+++ hello-word.py
@@ -1,1 +1,6 @@
-print("Hola Mundo desde GitHub Actions! 🚀")
+import os
+
+# Leemos la variable de entorno que definiremos en el YAML
+nombre = os.getenv('VAR_NOMBRE', 'Usuario')
+
+print(f"Hola {nombre}, saludos desde Github actions")
